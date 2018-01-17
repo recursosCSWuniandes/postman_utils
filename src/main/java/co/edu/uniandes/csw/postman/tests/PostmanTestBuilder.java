68a01a587d9597b377c8238148d70b957a618e56
alejandroquintero <a.quintero10@uniandes.edu.co>
2018-01-17 09:43:50 -0500

    se elimina io exception

--- src/main/java/co/edu/uniandes/csw/postman/tests/PostmanTestBuilder.java
+++ src/main/java/co/edu/uniandes/csw/postman/tests/PostmanTestBuilder.java
@@ -91,11 +91,11 @@ public class PostmanTestBuilder {
         if(path.validateDir())
        for(File f : path.getFiles()){
            cb = new CollectionBuilder(f);
-           if(cb.isCollection(collectionName)){      
+           if(cb.isCollection(collectionName)){   
              coll = coll.concat(path.getPATH().concat("\\").concat(cb.getOriginalName()));
-           } else {
-              throw new IOException();
-           }
+           }// else {
+            //  throw new IOException();
+          // }
        }
         tmp = File.createTempFile(collectionName, ".bat");
         bw = new BufferedWriter(new FileWriter(tmp));
@@ -231,4 +231,5 @@ public class PostmanTestBuilder {
     public String getPrerequest_scripts_failed() {
         return prerequest_scripts_failed;
     }
+   
 }
